9417001507328e926ab2de89cd9ac4b3999e04ab
Argom13 <diegoandresfajardoromero@gmail.com>
2026-01-22 22:02:13 -0600

    Se añadió scripts, cambios en html y css

---
+++ docs/scripts/script.js
@@ -0,0 +1,33 @@
+// Add Event Listener for DOMContentLoaded
+let formData = {};
+document.addEventListener("DOMContentLoaded", ()=>{
+    initFormValidation();
+});
+
+function initFormValidation(){
+    formData.txtNombre = document.querySelector("#txtNombre");
+    formData.txtEmail = document.querySelector("#txtEmail");
+    formData.txtComment = document.querySelector("#txtMensaje");
+    formData.btnEnviar = document.querySelector("#btnEnviar");
+    formData.btnEnviar.addEventListener("click", validarFormulario);
+}
+const isEmptyRegex = /^\s*$/;
+const isValidEmail = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+function validarFormulario(e) {
+    e.stopPropagation();
+    let isValid = true;
+    let emailValue = formData.txtEmail.value;
+    let nameValue = formData.txtNombre.value;
+    if ( isEmptyRegex.test(nameValue)) {
+        alert("El Nombre es requerido");
+        isValid = false;
+    }
+    if ( !isValidEmail.test(emailValue)) {
+        alert( "El correo no es válido!");
+        isValid = false;
+    }
+
+    if (!isValid) {
+        e.preventDefault();
+    }
+}
